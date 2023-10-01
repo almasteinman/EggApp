44610cd92ed6a8dcd2f16fdde1f382ac71d7de25
almasteinman <alma.steinman@gmail.com>
2023-10-01 17:03:27 +0300

    image changing when counter is 0

--- app/src/main/java/com/example/eggapp/MainActivity.java
+++ app/src/main/java/com/example/eggapp/MainActivity.java
@@ -3,13 +3,35 @@ package com.example.eggapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int counter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hey");
+        counter = 10;
+        TextView counterTextView = findViewById(R.id.textview_counter);
+        counterTextView.setText(""+counter);
+    }
+
+    public void knock(View view) {
+        if (counter > 0) {
+            counter--;
+            TextView counterTextView = findViewById(R.id.textview_counter);
+            counterTextView.setText(""+counter);
+            if (counter == 0) {
+                ImageView eggImageView = findViewById(R.id.imageview_egg);
+                eggImageView.setImageResource(R.drawable.donaldduck);
+            }
+        }
+    }
+
+
+    public void reset(View view) {
     }
 }
